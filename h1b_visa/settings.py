d6a429c79b65d5fa2929a759fc4bced6e94a1e51
raasheedhaa03 <116292222+raasheedhaa03@users.noreply.github.com>
2023-03-16 22:56:44 +0530

    Initial Commit

--- h1b_visa/settings.py
+++ h1b_visa/settings.py
@@ -118,12 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 import os
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, '/static/'),
 ]
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
