bf65f8ee4883fe93aeb2abee028c6186e873e515
raasheedhaa03 <116292222+raasheedhaa03@users.noreply.github.com>
2023-03-17 23:12:58 +0530

    Initial Commit

--- h1b_visa/settings.py
+++ h1b_visa/settings.py
@@ -125,10 +125,10 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '/static/'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATIC_URL = '/static/'
 
 # Default primary key field type
