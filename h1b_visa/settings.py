3a991a0600757d0b551f1a1a1291a01f1cac531a
raasheedhaa03 <116292222+raasheedhaa03@users.noreply.github.com>
2023-03-17 23:25:44 +0530

    Initial Commit

--- h1b_visa/settings.py
+++ h1b_visa/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/static/'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
