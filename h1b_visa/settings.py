db48292ba068729f9250fd25d1b7bbeccf15a80b
raasheedhaa03 <116292222+raasheedhaa03@users.noreply.github.com>
2023-03-17 19:10:51 +0530

    Initial Commit

--- h1b_visa/settings.py
+++ h1b_visa/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = '/opt/render/project/static'
+STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/static/'),
 ]
