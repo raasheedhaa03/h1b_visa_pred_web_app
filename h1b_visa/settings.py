ff5efdf2375c4eb6e412f331e6abd1bef185da01
raasheedhaa03 <116292222+raasheedhaa03@users.noreply.github.com>
2023-03-21 13:11:03 +0530

    Initial Commit

--- h1b_visa/settings.py
+++ h1b_visa/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 
 STATICFILES_DIRS = [
